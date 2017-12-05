743f6f8dc40679f9f74526517de91585e076354f
rrobby86 <rrobby86@gmail.com>
2017-12-05 10:05:22 +0100

    Multiple formats support, ascii format

--- pyplpr.py
+++ pyplpr.py
@@ -18,7 +18,8 @@ def provider_downames_python(country):
 def provider_holidays_enrico(country, year):
     from urllib import request
     import json
-    url = "http://kayaposoft.com/enrico/json/v1.0/?action=getPublicHolidaysForYear&year={}&country={}&region=" \
+    url = "http://kayaposoft.com/enrico/json/v1.0/" \
+            "?action=getPublicHolidaysForYear&year={}&country={}&region=" \
             .format(year, country)
     with request.urlopen(url) as url:
         data = json.loads(url.read().decode())
@@ -26,7 +27,7 @@ def provider_holidays_enrico(country, year):
             for h in data}
 
 def locate_provider(key, name):
-    return globals()["provider_{}_{}".format(key, name)]
+    return globals()["{}_{}".format(key, name)]
 
 def make_data(year, country=None, providers={}):
     from datetime import date
@@ -35,18 +36,27 @@ def make_data(year, country=None, providers={}):
     if not country:
         import locale
         country, _ = locale.getdefaultlocale()
-    data = {key: locate_provider(key, prov)(country)
+    data = {key: locate_provider("provider_" + key, prov)(country)
             for key, prov in providers.items()}
     leap = year%4 == 0 and (year%100 != 0 or year%400 == 0)
-    data["daycount"] = [31, 29 if leap else 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-    data["firstdow"] = [date(year, month, 1).weekday() for month in range(1, 13)]
+    data["daycount"] = [31, 29 if leap else 28, 31, 30, 31, 30,
+                        31, 31, 30, 31, 30, 31]
+    data["firstdow"] = [date(year, month, 1).weekday()
+                        for month in range(1, 13)]
     return data
 
-def renderer_html(out, data):
-    from jinja2 import Environment, PackageLoader
-    env = Environment(loader=PackageLoader(__name__, 'templates'))
-    template = env.get_template("template.html")
-    template.stream(data).dump(out)
+def jinja_renderer(template_file, **env_options):
+    def result(out, data):
+        from jinja2 import Environment, PackageLoader
+        env = Environment(loader=PackageLoader(__name__, "templates"),
+                **env_options)
+        template = env.get_template(template_file)
+        template.stream(data).dump(out)
+    return result
+
+renderer_html = jinja_renderer("template.html", trim_blocks=True)
+
+renderer_ascii = jinja_renderer("ascii.txt", trim_blocks=True)
 
 if __name__ == "__main__":
     from argparse import ArgumentParser
@@ -57,6 +67,8 @@ if __name__ == "__main__":
                         help="calendar locale (default: system locale)")
     parser.add_argument("-n", "--holidays",
                         help="include holidays of specified nation")
+    parser.add_argument("-f", "--format", default="html",
+                        help="output format (default: html)")
     parser.add_argument("-o", "--output",
                         help="name of output file (default: standard output)")
     args = parser.parse_args()
@@ -66,9 +78,10 @@ if __name__ == "__main__":
     data = make_data(args.year, args.locale)
     if args.holidays:
         data["holidays"] = provider_holidays_enrico(args.holidays, args.year)
+    renderer = locate_provider("renderer", args.format)
     if args.output:
         with open(args.output, "wt") as output:
-            renderer_html(output, data)
+            renderer(output, data)
     else:
         import sys
-        renderer_html(sys.stdout, data)
+        renderer(sys.stdout, data)
