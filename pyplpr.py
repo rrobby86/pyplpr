2872a6497c0fd0b7c65eef84e7d21f2289783d21
rrobby86 <rrobby86@gmail.com>
2017-12-08 16:21:25 +0100

    Merged name providers

--- pyplpr.py
+++ pyplpr.py
@@ -1,6 +1,6 @@
 default_providers = {
-    "monthnames": "python",
-    "downames": "python"
+    "names": "python",
+    "holidays": "enrico"
 }
 
 def urlopen(*args, **kwargs):
@@ -17,17 +17,16 @@ def urlopen(*args, **kwargs):
         _urlopen = dourlopen
     return _urlopen(*args, **kwargs)
 
-def provider_monthnames_python(country):
+def provider_names_python(country=None):
     import locale
+    if not country:
+        country, _ = locale.getdefaultlocale()
     locale.setlocale(locale.LC_ALL, (country, "UTF-8"))
-    return [locale.nl_langinfo(getattr(locale, "MON_" + str(i)))
-            for i in range(1, 13)]
-
-def provider_downames_python(country):
-    import locale
-    locale.setlocale(locale.LC_ALL, (country, "UTF-8"))
-    return [locale.nl_langinfo(getattr(locale, "DAY_" + str(i%7+1)))
-            for i in range(1, 8)]
+    months = tuple(locale.nl_langinfo(getattr(locale, "MON_" + str(i)))
+            for i in range(1, 13))
+    dows = tuple(locale.nl_langinfo(getattr(locale, "DAY_" + str(i%7+1)))
+            for i in range(1, 8))
+    return months, dows
 
 def provider_holidays_enrico(country, year):
     import json
@@ -42,16 +41,10 @@ def provider_holidays_enrico(country, year):
 def locate_provider(key, name):
     return globals()["{}_{}".format(key, name)]
 
-def make_data(year, country=None, providers={}):
+def calendar_data(year):
     from datetime import date
-    providers = providers.copy()
-    providers.update(default_providers)
-    if not country:
-        import locale
-        country, _ = locale.getdefaultlocale()
-    data = {key: locate_provider("provider_" + key, prov)(country)
-            for key, prov in providers.items()}
     leap = year%4 == 0 and (year%100 != 0 or year%400 == 0)
+    data = {}
     data["daycount"] = [31, 29 if leap else 28, 31, 30, 31, 30,
                         31, 31, 30, 31, 30, 31]
     data["firstdow"] = [date(year, month, 1).weekday()
@@ -108,7 +101,8 @@ if __name__ == "__main__":
     if not args.year:
         from datetime import date, timedelta
         args.year = (date.today() + timedelta(days=31)).year
-    data = make_data(args.year, args.locale)
+    data = calendar_data(args.year)
+    data["monthnames"], data["downames"] = provider_names_python(args.locale)
     if args.holidays:
         data["holidays"] = provider_holidays_enrico(args.holidays, args.year)
     renderer = locate_provider("renderer", args.format)
